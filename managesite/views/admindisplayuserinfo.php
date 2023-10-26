965d18403f265fd4fa8cb01f6619f7e00c4c3bb5
Mac Mini <macmini@Macs-Mac-mini.local>
2023-10-26 17:43:24 +0600

    Added More info on user for admin

--- managesite/views/admindisplayuserinfo.php
+++ managesite/views/admindisplayuserinfo.php
@@ -6,26 +6,32 @@ if( count( $usersData ) > 0 ) {
     $usersDataHtml = '<table>  
                     <thead> 
                         <tr> 
+                            <th>Name</th>
                             <th>Username</th>
+                            <th>Mail</th>
                             <th>Make Admin</th>
                             <th>Admin Level</th>
                             <th>Delete from Admin</th>
+                            <th>Action</th>
                         </tr>
                     </thead>';
     foreach ($usersData as $key => $userData) {
         //    $usersDataHtml .= '<div class=""> <span class="">'.$userData['username'].'</span> </div>';
-        $usersDataHtml .= '<tr id="' . $userData['userkey'] . '">
-                                                <td>' . $userData['username'] . '</td>
-                                                <td><input type="checkbox"></td>
-                                                <td>
-                                                    <select>
-                                                        <option value="superadmin">Super Admin</option>
-                                                        <option value="admin">Admin</option>
-                                                        <option value="user">Modaretor</option>
-                                                    </select>
-                                                </td>
-                                                <td><button>Delete</button></td>
-                                            </tr>';
+        $usersDataHtml .= '<tr id="user_' . $userData['userkey'] . '">
+                                <td>' . $userData['first_name'].' '.$userData['last_name']. '</td>
+                                <td>' . $userData['username'] . '</td>
+                                <td>' . $userData['mail'] . '</td>
+                                <td><input type="checkbox"></td>
+                                <td>
+                                    <select>
+                                        <option value="superadmin">Super Admin</option>
+                                        <option value="admin">Admin</option>
+                                        <option value="user">Modaretor</option>
+                                    </select>
+                                </td>
+                                <td><button>Delete</button></td>
+                                <td><button id="'.$userData['userkey'].'">Submit</button></td>
+                            </tr>';
     }
 
     $usersDataHtml .= '</tbody></table>';
