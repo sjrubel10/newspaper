98507ace31a622f2782aa7e3367f84b9a9bd075f
“Rubel” <rubelcuet10@gmail.com>
2024-01-17 16:35:43 +0600

    Modify js code for editor

--- texteditor.php
+++ texteditor.php
@@ -33,7 +33,6 @@ function display_rich_text_editor_toolbar(){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <title>Text Editor</title>
     <style>
@@ -94,9 +93,9 @@ function display_rich_text_editor_toolbar(){
 </head>
 <body>
 <form >
-    <div class="editor-container">
-        <span class="editorText">Description</span>
-        <?php echo display_rich_text_editor_toolbar(); ?>
+    <div class="editor-container" id="editor-container">
+        <span class="editorText" id="editorText">Description</span>
+<!--        --><?php //echo display_rich_text_editor_toolbar(); ?>
         <div contenteditable="true" class="editor" id="editor">
             <p> Text Here </p>
         </div>
@@ -116,36 +115,34 @@ function display_rich_text_editor_toolbar(){
 
         let isClicked = 0
         $('#editor').on('focus', function() {
-
             if( isClicked === 0 ){
-                // $("#editor").prepend( display_rich_text_editor_toolbar() );
+                $("#editorText").after( display_rich_text_editor_toolbar() );
                 $("#editortoolbar").fadeIn(1000);
             }
             isClicked++;
-
         });
 
         function display_rich_text_editor_toolbar() {
             let toolbar = "<div class='toolbar'>\
-                            <button id='bold-button' class='box_padding'><i class='fas fa-bold'></i></button>\
-                            <button id='italic-button' class='box_padding'><i class='fas fa-italic'></i></button>\
-                            <button id='underline-button' class='box_padding'><i class='fas fa-underline'></i></button>\
-                            <button id='p-button' class='box_padding'><i class='fas fa-paragraph'></i></button>\
-                            <button id='h1-button' class='box_padding'><i class='fas fa-heading'></i>1</button>\
-                            <button id='h2-button' class='box_padding'><i class='fas fa-heading'></i>2</button>\
-                            <input type='color' class='color_picker' id='text-color-picker'>\
-                            <button id='multiply-button' class='box_padding'><i class='fas fa-superscript'></i>2</button>\
-                            <button id='cube-button' class='box_padding'><i class='fas fa-superscript'></i>3</button>\
-                            <button id='square-root-button' class='box_padding'><i class='fas fa-square-root-alt'></i></button>\
-                            <button id='left-align-button' class='box_padding'><i class='fas fa-align-left'></i></button>\
-                            <button id='center-align-button' class='box_padding'><i class='fas fa-align-center'></i></button>\
-                            <button id='right-align-button' class='box_padding'><i class='fas fa-align-right'></i></button>\
-                            <button id='ordered-list-button' class='box_padding'><i class='fas fa-list-ol'></i></button>\
-                            <button id='link-button' class='box_padding'><i class='fas fa-link'></i></button>\
-                            <button id='unlink-button' class='box_padding'><i class='fas fa-unlink'></i></button>\
+                            <button id='bold-button' class='box_padding btnClickedClass'><i class='fas fa-bold'></i></button>\
+                            <button id='italic-button' class='box_padding btnClickedClass'><i class='fas fa-italic'></i></button>\
+                            <button id='underline-button' class='box_padding btnClickedClass'><i class='fas fa-underline'></i></button>\
+                            <button id='p-button' class='box_padding btnClickedClass'><i class='fas fa-paragraph'></i></button>\
+                            <button id='h1-button' class='box_padding btnClickedClass'><i class='fas fa-heading'></i>1</button>\
+                            <button id='h2-button' class='box_padding btnClickedClass'><i class='fas fa-heading'></i>2</button>\
+                            <input type='color' class='color_picker btnClickedClass' id='text-color-picker'>\
+                            <button id='multiply-button' class='box_padding btnClickedClass'><i class='fas fa-superscript'></i>2</button>\
+                            <button id='cube-button' class='box_padding btnClickedClass'><i class='fas fa-superscript'></i>3</button>\
+                            <button id='square-root-button' class='box_padding btnClickedClass'><i class='fas fa-square-root-alt'></i></button>\
+                            <button id='left-align-button' class='box_padding btnClickedClass'><i class='fas fa-align-left'></i></button>\
+                            <button id='center-align-button' class='box_padding btnClickedClass'><i class='fas fa-align-center'></i></button>\
+                            <button id='right-align-button' class='box_padding btnClickedClass'><i class='fas fa-align-right'></i></button>\
+                            <button id='ordered-list-button' class='box_padding btnClickedClass'><i class='fas fa-list-ol'></i></button>\
+                            <button id='link-button' class='box_padding btnClickedClass'><i class='fas fa-link'></i></button>\
+                            <button id='unlink-button' class='box_padding btnClickedClass'><i class='fas fa-unlink'></i></button>\
                             <button id='insert_image'><i class='fas fa-image'></i></button>\
                             <button id='insert_table'><i class='fas fa-table'></i></button>\
-                            <button id='clean-button' class='box_padding'><i class='fas fa-trash'></i></button>\
+                            <button id='clean-button' class='box_padding btnClickedClass'><i class='fas fa-trash'></i></button>\
                         </div>";
 
             return toolbar;
@@ -157,7 +154,7 @@ function display_rich_text_editor_toolbar(){
             $(this).html("<p>" + sanitizedContent + "</p>");*/
         });
 
-        $("#clean-button").click(function( event ) {
+        $('body').on('click', '#clean-button', function( event ) {
             event.preventDefault();
             var content = $("#editor").html();
             // Remove CSS styles (within <style> tags)
@@ -177,84 +174,150 @@ function display_rich_text_editor_toolbar(){
             document.execCommand( action, arg1, arg2);
         }
 
-        $("#bold-button").click(function( event ) {
+        $('body').on('click', '.btnClickedClassqq', function( event ) {
+            event.preventDefault();
+            /*let clickedId = $(this).attr('id');
+            alert( clickedId );*/
+
+            var buttonId = $(this).attr("id");
+
+            if (buttonId === "bold-button") {
+                make_edit( event, "bold", false, null );
+            } else if (buttonId === "italic-button") {
+                make_edit( event, "italic", false, null );
+            } else if (buttonId === "underline-button") {
+                make_edit( event, "underline", false, null );
+            } else if (buttonId === "p-button") {
+                make_edit( event, "formatBlock", false, "<p>" );
+            } else if (buttonId === "h1-button") {
+                make_edit( event, "formatBlock", false, "<h1>" );
+            } else if (buttonId === "h2-button") {
+                make_edit( event, "formatBlock", false, "<h2>" );
+            } else if (buttonId === "text-color-picker") {
+                var color = $("#text-color-picker").val();
+                make_edit( event, "foreColor", false, color);
+            } else if (buttonId === "multiply-button") {
+                make_edit( event, "insertHTML", false, "<sup>2</sup>" );
+            } else if (buttonId === "cube-button") {
+                make_edit( event, "insertHTML", false, "<sup>3</sup>" );
+            } else if (buttonId === "square-root-button") {
+                make_edit( event, "insertHTML", false, "√" );
+            } else if (buttonId === "left-align-button") {
+                make_edit( event, "justifyLeft", false, null );
+            } else if (buttonId === "center-align-button") {
+                make_edit( event, "justifyCenter", false, null );
+            } else if (buttonId === "right-align-button") {
+                make_edit( event, "justifyRight", false, null );
+            } else if (buttonId === "ordered-list-button") {
+                make_edit( event, "insertOrderedList", false, null );
+            } else if (buttonId === "link-button") {
+                var url = prompt("Enter a URL:");
+                if (url) {
+                    make_edit( event, "createLink", false, url );
+                }
+            } else if (buttonId === "unlink-button") {
+                make_edit( event, "unlink", false, null );
+            } else if (buttonId === "insert_image") {
+                insertImage( event );
+            } else if (buttonId === "insert_table") {
+                insertTable( event );
+            } else if (buttonId === "clean-button") {
+                var content = $("#editor").html();
+                // Remove CSS styles (within <style> tags)
+                content = content.replace(/<style>[\s\S]*?<\/style>/gi, '');
+                // Remove HTML entities
+                content = $("<textarea/>").html(content).text();
+                // Remove all HTML tags
+                content = content.replace(/<[^>]+>/g, '');
+                // Wrap the clean text in a <p> tag
+                var result = '<p>' + content + '</p';
+                // Replace the content of the div with the cleaned HTML
+                $("#editor").html(result);
+            }
+
+        });
+
+        $('body').on('click', '#bold-button', function( event ) {
             make_edit( event, "bold", false, null );
         });
 
-        $("#italic-button").click(function( event ) {
+        $('body').on('click', '#italic-button', function( event ) {
             make_edit( event, "italic", false, null );
         });
 
-        $("#underline-button").click(function( event ) {
+        $('body').on('click', '#underline-button', function( event ) {
             make_edit( event, "underline", false, null );
         });
 
-        $("#p-button").click(function( event ) {
+        $('body').on('click', '#p-button', function( event ) {
             make_edit( event, "formatBlock", false, "<p>" );
         });
-        $("#h1-button").click(function( event ) {
+
+        $('body').on('click', '#h1-button', function( event ) {
             make_edit( event, "formatBlock", false, "<h1>" );
         });
 
-        $("#h2-button").click(function( event ) {
+        $('body').on('click', '#h2-button', function( event ) {
             make_edit( event, "formatBlock", false, "<h2>" );
         });
 
-        $("#text-color-picker").change(function( event ) {
+        $('body').on('change', '#text-color-picker', function( event ) {
             var color = $("#text-color-picker").val();
             make_edit( event, "foreColor", false, color);
         });
 
-        $("#font-size-select").change(function( event ) {
+        $('body').on('change', '#font-size-select', function( event ) {
             var fontSize = $("#font-size-select").val();
             make_edit( event, "fontSize", false, fontSize);
         });
 
-        $("#square-root-button").click(function( event ) {
+        $('body').on('click', '#square-root-button', function( event ) {
             make_edit( event, "insertHTML", false, "√" );
         });
 
-        $("#multiply-button").click(function( event ) {
+        $('body').on('click', '#multiply-button', function( event ) {
             make_edit( event, "insertHTML", false, "<sup>2</sup>" );
         });
 
-        $("#cube-button").click(function( event ) {
+        $('body').on('click', '#cube-button', function( event ) {
             make_edit( event, "insertHTML", false, "<sup>3</sup>" );
         });
 
-        $("#left-align-button").click(function( event ) {
+        $('body').on('click', '#left-align-button', function( event ) {
             make_edit( event, "justifyLeft", false, null );
         });
 
-        $("#center-align-button").click(function( event ) {
+        $('body').on('click', '#center-align-button', function( event ) {
             make_edit( event, "justifyCenter", false, null );
         });
 
-        $("#right-align-button").click(function( event ) {
+        $('body').on('click', '#right-align-button', function( event ) {
             make_edit( event, "justifyRight", false, null );
         });
 
-        $("#ordered-list-button").click(function( event ) {
+        $('body').on('click', '#ordered-list-button', function( event ) {
             make_edit( event, "insertOrderedList", false, null );
         });
 
-        $("#link-button").click(function( event ) {
+        $('body').on('click', '#link-button', function( event ) {
+            event.preventDefault();
             var url = prompt("Enter a URL:");
             if (url) {
                 make_edit( event, "createLink", false, url );
             }
         });
 
-        $("#unlink-button").click(function( event ) {
+        $('body').on('click', '#unlink-button', function( event ) {
             make_edit( event, "unlink", false, null );
         });
-        $("#insert_image").click(function( event ) {
+
+        $('body').on('click', '#insert_image', function( event ) {
             insertImage( event );
         });
-        $("#insert_table").click(function( event ) {
+
+        $('body').on('click', '#insert_table', function( event ) {
             insertTable( event );
         });
-
         function insertImage( event ) {
             event.preventDefault();
             var url = prompt('Enter the image URL:');
@@ -281,6 +344,8 @@ function display_rich_text_editor_toolbar(){
                 document.execCommand('insertHTML', false, tableHTML);
             }
         }
+
+
 
         $(".submitaa").click(function( event ) {
             event.preventDefault();
