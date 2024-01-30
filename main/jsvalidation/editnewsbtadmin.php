bfa7be22bae1ffae4af3054bcf949465acf2ec4c
“Rubel” <rubelcuet10@gmail.com>
2024-01-30 11:42:08 +0600

    First commit in newspaper for edit posts image

--- main/jsvalidation/editnewsbtadmin.php
+++ main/jsvalidation/editnewsbtadmin.php
@@ -3,23 +3,18 @@ require_once "../init.php";
 if( isset( $_SESSION['logged_in'] ) && $_SESSION['logged_in'] ){
     if( $_SESSION['logged_in_user_data']['admin']=== 1 ) {
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $title = isset($_POST["title"]) ? trim($_POST["title"]):"";
-            $newkey = substr(md5($title), 0, 8);;
-            $description = isset($_POST["description"]) ? $_POST["description"]: "";
-            $category = isset($_POST["category"]) ? $_POST["category"]:"";
+            $title = isset( $_POST["title"] ) ? trim( $_POST["title"] ) : "";
+            $newkey = substr( md5( $title ), 0, 8 );;
+            $description = isset( $_POST["description"] ) ? $_POST["description"]: "";
+            $category = isset( $_POST["category"] ) ? sanitize( $_POST["category"] ): "";
+            $imageFileName = isset( $_POST["postImage"] ) ? sanitize( $_POST["postImage"] ) : Null;
+            $additional_images = isset( $_POST["postGalleryImage"] ) ? sanitize( $_POST["postGalleryImage"] ) : Null;
             $userid = 1;
 
             $newskey = $_POST['newskey'];
             $conn = Db_connect();
-            // Handle image upload
-            $timestamp = time(); // Get current timestamp
-            $imageFileName = $timestamp . '_' . basename($_FILES["images"]["name"]); // Append timestamp to the image file name
-            $targetDir = "../../assets/uploads/";
-            $targetFile = $targetDir . $imageFileName;
             $newsid = 1;
-            $result = updateNews( $title, $description, $imageFileName, $category, $newskey );
-            move_uploaded_file($_FILES["images"]["tmp_name"], $targetFile);
-            //    resizeAndSaveImage($_FILES["images"]["tmp_name"], $targetFile);
+            $result = updateNews( $title, $description, $imageFileName, $additional_images, $category, $newskey );
         } else {
             $result = array(
                 'success' => false,
