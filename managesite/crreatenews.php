c64415a77c5419d3e86506f56e09fd82314c9492
“Rubel” <rubelcuet10@gmail.com>
2024-02-02 13:17:08 +0600

    medda added with image new form

--- managesite/crreatenews.php
+++ managesite/crreatenews.php
@@ -11,63 +11,60 @@
     <link rel="stylesheet" href="../assets/css/common.css">
     <link rel="stylesheet" href="adminassets/css/createnews.css">
     <link rel="stylesheet" href="../assets/css/texteditor.css">
-
     <link rel="stylesheet" href="../assets/css/mediafile.css">
 
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="../assets/js/common.js"></script>
-    <script src="../assets/js/texteditor.js"></script>
-    <script src="../assets/js/createnew.js"></script>
+
 </head>
 <body>
 <?php include_once "../views/header.php"?>
 <h1 class="createNewsTitle">Create Products</h1>
 <div class="formHolder">
     <div class='container'>
-    <div class='left-side'>
+        <div class='left-side'>
 
-        <div class='section input-section'>
-            <h3 class='section-title'>Title</h3>
-            <input class='text-input' type='text' name='title' placeholder='Product Title' />
-        </div>
-
-        <div class='section input-section'>
-            <h3 class='section-title'>Description</h3>
-            <textarea class='text-input' rows='8' type='text' name='description' placeholder='Product Description'></textarea>
-        </div>
-
-        <div class='section input-section'>
-            <h3 class='section-title'>Short Description</h3>
-            <textarea class='text-input' rows='5' type='text' name='description' placeholder='Product Short Description'></textarea>
-        </div>
-
-        <div class='section input-section'>
-            <h3 class='section-title'>Product Type</h3>
-            <div class='input-group'>
-                <input class='radio-input' name='product-type' type='radio' placeholder='Product Sku'/>
-                <label class='radio-label'>
-                    Simple
-                </label>
+            <div class='section input-section'>
+                <h3 class='section-title'>Title</h3>
+                <input class='text-input' type='text' name='title' placeholder='Product Title' />
             </div>
 
-            <div class='input-group'>
-                <input class='radio-input' name='product-type' type='radio' placeholder='Product Sku'/>
-                <label class='radio-label'>
-                    Variable
-                </label>
+            <div class='section input-section'>
+                <h3 class='section-title'>Description</h3>
+                <textarea class='text-input' rows='8' type='text' name='description' placeholder='Product Description'></textarea>
             </div>
 
-            <div class='input-group'>
-                <input class='radio-input' name='product-type' type='radio' placeholder='Product Sku'/>
-                <label class='radio-label'>
-                    Complex
-                </label>
+            <div class='section input-section'>
+                <h3 class='section-title'>Short Description</h3>
+                <textarea class='text-input' rows='5' type='text' name='description' placeholder='Product Short Description'></textarea>
             </div>
+
+            <div class='section input-section'>
+                <h3 class='section-title'>Product Type</h3>
+                <div class='input-group'>
+                    <input class='radio-input' name='product-type' type='radio' placeholder='Product Sku'/>
+                    <label class='radio-label'>
+                        Simple
+                    </label>
+                </div>
+
+                <div class='input-group'>
+                    <input class='radio-input' name='product-type' type='radio' placeholder='Product Sku'/>
+                    <label class='radio-label'>
+                        Variable
+                    </label>
+                </div>
+
+                <div class='input-group'>
+                    <input class='radio-input' name='product-type' type='radio' placeholder='Product Sku'/>
+                    <label class='radio-label'>
+                        Complex
+                    </label>
+                </div>
+            </div>
+
         </div>
-
-    </div>
-    <div class='right-side'>
+        <div class='right-side'>
         <div class='section'>
             <h3 class='section-title'>Cover Image</h3>
             <img class='cover-image' src="https://images.pexels.com/photos/335257/pexels-photo-335257.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="cover image">
@@ -143,7 +140,12 @@
             <input class='text-input' type='text' placeholder='Product Stock' />
         </div>
     </div>
-</div>
+    </div>
 </div>
 </body>
 </html>
+
+<script src="../assets/js/common.js"></script>
+<script src="../assets/js/texteditor.js"></script>
+<script src="../assets/js/createnew.js"></script>
+<script src="../assets/js/addmedia.js"></script>
